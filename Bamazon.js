461ebb5bbc1a7ae6c1268af69921c16cedd631f2
Bruno Marques <brunomarques@Brunos-MacBook-Pro.local>
2016-04-29 16:19:29 -0400

    more stuff

--- Bamazon.js
+++ Bamazon.js
@@ -1,28 +1,61 @@
 var prompt     = require('prompt');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  database : 'bamazon'
+	host     : 'localhost',
+	user     : 'root',
+	database : 'bamazon'
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
- 
-  console.log('The solution is: ', rows[0].solution);
-}); 
 
-connection.query('select * from bamazon.Products', function(err,result){
-	if(err) throw err; 
-	console.log(result);
+var showProducts = function(){
 
+	connection.query('select ItemId,ProductName,Price from bamazon.Products', function(err,result){
+		if(err) throw err; 
+
+		
+		console.log(result); 
+		
+
+	}); 
+	
+
+		prompt.start() 
+		prompt.get(ItemId,stockQTY, function (err,result){
+			if(err) throw err
+				var userProduct = res.ItemId;
+			var userAvailable =res.stockQTY; 
+
+			userPurchase(userProduct,userAvailable); 
+			start=false 
+			showProducts();
+		});
+	} 
+
+
+var purchaseCheck = function(itemid,quantity){
+	connection.query('select * from bamazon.Products where ItemId=' + itemid, function(err,result){
+		if (err) throw err; 
+		var productCheck= result[0].StockQTY - quantity; 
+
+		if(productCheck < 0){
+			console.log('We are all out :(')
+		}else{
+			console.log('We got' + result[0].ProductName +'left') 
+			updateDB(itemid,res[0].StockQTY);
+		}
+	});
+};
+
+var updateDB = function(itemid,quantity){
+	quantity=quantity-1; 
+	console.log(quantity) 
+	connection.query(' update bamazon.Products set ItemId '+ quantity + 'where ItemId' +itemid, function(err,result){
+		if (err) throw err;
+	});
+}
+
+connection.connect(function(err){
+	if (err) throw err; 
 }); 
-// prompt.start() 
-// prompt.get(ItemId,stockQTY, function (err,result){
-// 	if(err) 
-// 		console.log(err)
-// }else
- 
-// connection.end();
+showProducts();
+
+connection.end();
